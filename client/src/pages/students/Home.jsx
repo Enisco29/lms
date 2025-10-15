05dd1fddf50f24a340c5a3a1cad1929e14cb75f9
Enisco29 <opeyemioluwafisayo29@gmail.com>
2025-10-15 23:27:45 +0100

    added callToAction section

--- client/src/pages/students/Home.jsx
+++ client/src/pages/students/Home.jsx
@@ -1,3 +1,4 @@
+import CallToAction from "../../components/students/CallToAction";
 import Companies from "../../components/students/Companies";
 import Coursesection from "../../components/students/Coursesection";
 import Hero from "../../components/students/Hero";
@@ -7,9 +8,10 @@ const Home = () => {
   return (
     <div className="flex flex-col items-center space-y-7 text-center">
       <Hero />
-      <Companies/>
-      <Coursesection/>
-      <Testimonials/>
+      <Companies />
+      <Coursesection />
+      <Testimonials />
+      <CallToAction />
     </div>
   );
 };
