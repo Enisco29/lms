28a57cac7b8e8c55a8579560093dbea7cab044e8
Enisco29 <opeyemioluwafisayo29@gmail.com>
2025-10-16 23:56:01 +0100

    feat: finished the course details design

--- client/src/pages/students/CourseDetails.jsx
+++ client/src/pages/students/CourseDetails.jsx
@@ -4,14 +4,17 @@ import { AppContext } from "../../context/AppContext";
 import Loading from "../../components/students/Loading";
 import { assets } from "../../assets/assets";
 import humanizeDuration from "humanize-duration";
+import Footer from "../../components/students/Footer";
 
 const CourseDetails = () => {
   const { id } = useParams();
 
   const [courseData, setCourseData] = useState(null);
   const [openSection, setOpenSection] = useState({});
+  const [isAlreadyEnrolled, setIsAlreadyEnrolled] = useState(false);
 
   const {
+    currency,
     allCourses,
     calculateRating,
     calculateChapterTime,
@@ -91,9 +94,18 @@ const CourseDetails = () => {
                   key={index}
                   className="border border-gray-300 bg-white mb-2 rounded"
                 >
-                  <div className="flex items-center justify-between px-4 py-3 cursor-pointer select-none">
+                  <div
+                    onClick={() => toggleSection(index)}
+                    className="flex items-center justify-between px-4 py-3 cursor-pointer select-none"
+                  >
                     <div className="flex items-center gap-2">
-                      <img src={assets.down_arrow_icon} alt="arrow_icon" />
+                      <img
+                        src={assets.down_arrow_icon}
+                        alt="arrow_icon"
+                        className={`transform transition-transform ${
+                          openSection[index] ? "rotate-180" : ""
+                        }`}
+                      />
                       <p className="font-medium md:text-base text-sm">
                         {chapter.chapterTitle}
                       </p>
@@ -104,7 +116,11 @@ const CourseDetails = () => {
                     </p>
                   </div>
 
-                  <div className="overflow-hidden transition-all duration-300 max-h-96">
+                  <div
+                    className={`overflow-hidden transition-all duration-300 ${
+                      openSection[index] ? "max-h-96" : "max-h-0"
+                    } `}
+                  >
                     <ul className="list-disc md:pl-10 pl-4 pr-4 py-3 text-gray-600 border-t border-gray-300">
                       {chapter.chapterContent.map((lecture, i) => (
                         <li key={i} className="flex items-center gap-2 py-1">
@@ -137,11 +153,91 @@ const CourseDetails = () => {
               ))}
             </div>
           </div>
+
+          <div className="py-20 text-sm md:text-default">
+            <h3 className="text-xl font-semibold text-gary-800">
+              Course Description
+            </h3>
+            <p
+              className="pt-3 rich-text"
+              dangerouslySetInnerHTML={{
+                __html: courseData.courseDescription,
+              }}
+            ></p>
+          </div>
         </div>
 
         {/* right column */}
-        <div></div>
+        <div className="max-w-[424px] z-10 shadow-[0_4px_15px_2px] shadow-[rgba(0,0,0,0.1)] rounded-t md:rounded-none overflow-hidden bg-white min-w-[300px] sm:min-w-[420px]">
+          <img src={courseData.courseThumbnail} alt="thumbnail" />
+          <div className="p-5">
+            <div className="flex items-center gap-2">
+              <img
+                src={assets.time_left_clock_icon}
+                alt="time_icon"
+                className="w-3.5"
+              />
+              <p className="text-red-500">
+                <span className="font-medium">5 days</span>left at this price
+              </p>
+            </div>
+
+            <div className="flex gap-3 items-center pt-2">
+              <p className="text-gray-800 md:text-4xl text-2xl font-semibold">
+                {currency}
+                {(
+                  courseData.coursePrice -
+                  (courseData.discount * courseData.coursePrice) / 100
+                ).toFixed(2)}
+              </p>
+              <p className="md:text-lg text-gray-500 line-through">
+                {currency}
+                {courseData.coursePrice}
+              </p>
+              <p className="md:text-lg text-gray-500">
+                {courseData.discount}% off
+              </p>
+            </div>
+
+            <div className="flex items-center text-sm md:text-default gap-4 pt-2 md:pt-4 text-gray-500">
+              <div className="flex items-center gap-1">
+                <img src={assets.star} alt="star_icon" />
+                <p>{calculateRating(courseData)}</p>
+              </div>
+
+              <div className="flex items-center gap-1">
+                <img src={assets.time_clock_icon} alt="clock_icon" />
+                <p>{calculateCourseDuration(courseData)}</p>
+              </div>
+
+              <div className="h-4 w-px bg-gray-500/40"></div>
+
+              <div className="flex items-center gap-1">
+                <img src={assets.lesson_icon} alt="clock_icon" />
+                <p>{calculateNoOfLectures(courseData)} lessons</p>
+              </div>
+            </div>
+
+            <button className="md:mt-6 mt-4 w-full py-3 rounded bg-blue-600 text-white font-medium">
+              {isAlreadyEnrolled ? "Already Enrolled" : "Enroll Now"}
+            </button>
+
+            <div className="pt-6">
+              <p className="md:text-xl text-lg font-medium text-gray-800">
+                What's in the course?
+              </p>
+              <ul className="ml-4 pt-2 text-sm md:text-default list-disc text-gray-500">
+                <li>Lifetime accesss eith free updates</li>
+                <li>Lifetime accesss eith free updates</li>
+                <li>Lifetime accesss eith free updates</li>
+                <li>Lifetime accesss eith free updates</li>
+                <li>Lifetime accesss eith free updates</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
+      <Footer />
     </>
   ) : (
     <Loading />
