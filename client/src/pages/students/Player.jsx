a88b5342cfa28f7e50bbc3fa01d25d7046606b95
Enisco29 <opeyemioluwafisayo29@gmail.com>
2025-10-17 23:12:28 +0100

    faet: added player page

--- client/src/pages/students/Player.jsx
+++ client/src/pages/students/Player.jsx
@@ -1,9 +1,154 @@
-import React from 'react'
+import { useContext, useEffect, useState } from "react";
+import { AppContext } from "../../context/AppContext";
+import humanizeDuration from "humanize-duration";
+import Youtube from "react-youtube";
+import { useParams } from "react-router-dom";
+import { assets } from "../../assets/assets";
+import Footer from "../../components/students/Footer";
 
 const Player = () => {
-  return (
-    <div>Player</div>
-  )
-}
+  const { enrolledCourses, calculateChapterTime } = useContext(AppContext);
+  const { courseId } = useParams();
+  const [courseData, setCourseData] = useState(null);
+  const [openSection, setOpenSection] = useState({});
+  const [playerData, setPlayerData] = useState({});
 
-export default Player
+  const getCourseData = () => {
+    enrolledCourses.map((course) => {
+      if (course._id === courseId) {
+        setCourseData(course);
+      }
+    });
+  };
+
+  const toggleSection = (index) => {
+    setOpenSection((prev) => ({
+      ...prev,
+      [index]: !prev[index],
+    }));
+  };
+
+  useEffect(() => {
+    getCourseData();
+  }, [enrolledCourses]);
+
+  return (
+    <>
+      <div className="p-4 sm:p-10 flex flex-col-reverse md:grid md:grid-cols-2 gap-10 md:px-36">
+        {/* left column */}
+        <div className="text-gray-800">
+          <h2 className="text-sl font-semibold">Course Structure</h2>
+          <div className="pt-5">
+            {courseData &&
+              courseData.courseContent.map((chapter, index) => (
+                <div
+                  key={index}
+                  className="border border-gray-300 bg-white mb-2 rounded"
+                >
+                  <div
+                    onClick={() => toggleSection(index)}
+                    className="flex items-center justify-between px-4 py-3 cursor-pointer select-none"
+                  >
+                    <div className="flex items-center gap-2">
+                      <img
+                        src={assets.down_arrow_icon}
+                        alt="arrow_icon"
+                        className={`transform transition-transform ${
+                          openSection[index] ? "rotate-180" : ""
+                        }`}
+                      />
+                      <p className="font-medium md:text-base text-sm">
+                        {chapter.chapterTitle}
+                      </p>
+                    </div>
+                    <p className="text-sm md:text-lg">
+                      {chapter.chapterContent.length} lectures -{" "}
+                      {calculateChapterTime(chapter)}
+                    </p>
+                  </div>
+
+                  <div
+                    className={`overflow-hidden transition-all duration-300 ${
+                      openSection[index] ? "max-h-96" : "max-h-0"
+                    } `}
+                  >
+                    <ul className="list-disc md:pl-10 pl-4 pr-4 py-3 text-gray-600 border-t border-gray-300">
+                      {chapter.chapterContent.map((lecture, i) => (
+                        <li key={i} className="flex items-center gap-2 py-1">
+                          <img
+                            src={
+                              false ? assets.blue_tick_icon : assets.play_icon
+                            }
+                            alt="play_icon"
+                            className="w-4 h-4 cursor-pointer"
+                          />
+                          <div className="flex items-center justify-between w-full text-gray-800 text-xs md:text-default">
+                            <p>{lecture.lectureTitle}</p>
+                            <div className="flex gap-2">
+                              {lecture.lectureUrl && (
+                                <p
+                                  onClick={() =>
+                                    setPlayerData({
+                                      ...lecture,
+                                      chapter: index + 1,
+                                      lecture: i + 1,
+                                    })
+                                  }
+                                  className="text-blue-500 cursor-pointer"
+                                >
+                                  Watch
+                                </p>
+                              )}
+                              <p>
+                                {humanizeDuration(
+                                  lecture.lectureDuration * 60 * 1000,
+                                  { units: ["h", "m"] }
+                                )}
+                              </p>
+                            </div>
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              ))}
+          </div>
+
+          <div className="flex items-center gap-2 py-3 mt-10">
+            <h1 className="text-xl font-bold">Rate this Course</h1>
+          </div>
+        </div>
+
+        {/* right column */}
+        <div className="md:mt-10">
+          {playerData && playerData.lectureUrl ? (
+            <div>
+              <Youtube
+                videoId={playerData.lectureUrl.split("/").pop()}
+                iframeClassName="w-full aspect-video"
+              />
+              <div className="flex justify-between items-center mt-1">
+                <p>
+                  {playerData.chapter}.{playerData.lecture}{" "}
+                  {playerData.lectureTitle}
+                </p>
+                <button className="text-blue-600">
+                  {false ? "completed" : "Mark Complete"}
+                </button>
+              </div>
+            </div>
+          ) : (
+            <img
+              src={courseData ? courseData.courseThumbnail : ""}
+              alt="thumbnail"
+            />
+          )}
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Player;
