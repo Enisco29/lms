4e111566906f1ce61bdec652403abfcb525c576b
Enisco29 <opeyemioluwafisayo29@gmail.com>
2025-10-18 18:56:41 +0100

    craeted a navbar component for educator and integrated in educator page

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,11 +1,13 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "../../components/educator/Navbar";
 
 const Educator = () => {
   return (
     <>
-      <div>Educator</div>
-      <div>{<Outlet />}</div>
+      <div className="text-default min-h-screen bg-white">
+        <Navbar />
+        <div>{<Outlet />}</div>
+      </div>
     </>
   );
 };
