a7ea98c988be662760d9da21dcbec65fffcc201c
Enisco29 <opeyemioluwafisayo29@gmail.com>
2025-10-18 19:28:19 +0100

    feat: created edu footer & sidebar and mount it in the edu page

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,12 +1,21 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/educator/Navbar";
+import { useContext } from "react";
+import { AppContext } from "../../context/AppContext";
+import { assets } from "../../assets/assets";
+import Sidebar from "../../components/educator/Sidebar";
+import Footer from "../../components/educator/Footer";
 
 const Educator = () => {
   return (
     <>
       <div className="text-default min-h-screen bg-white">
         <Navbar />
-        <div>{<Outlet />}</div>
+        <div className="flex">
+          <Sidebar />
+          <div className="flex-1">{<Outlet />}</div>
+        </div>
+        <Footer />
       </div>
     </>
   );
