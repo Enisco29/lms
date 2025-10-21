b8293e0f206ad84e0b11b1b07a61a98c6bec9ae1
Enisco29 <opeyemioluwafisayo29@gmail.com>
2025-10-22 00:44:42 +0100

    feat: completed add course fro educator and middleware, dunno if it's working though

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,4 +1,6 @@
 import { clerkClient } from "@clerk/express";
+import Course from "../models/Course.js";
+import { v2 as cloudinary } from "cloudinary";
 
 //update role to educator
 export const updateRoleToEducator = async (req, res) => {
@@ -14,6 +16,39 @@ export const updateRoleToEducator = async (req, res) => {
     res.status(200).json({
       success: true,
       message: "You can publish a course now",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+//Add New Course
+export const addCourse = async (req, res) => {
+  try {
+    const { courseData } = req.body;
+    const imageFile = req.file;
+    const educatorId = req.auth.userId;
+
+    if (!imageFile) {
+      return res.status(400).json({
+        success: false,
+        message: "Thumbnail not attached",
+      });
+    }
+
+    const parsedCourseData = await JSON.parse(courseData);
+    parsedCourseData.educator = educatorId;
+    const newCourse = await Course.create(parsedCourseData);
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path);
+    newCourse.courseThumbnail = imageUpload.secure_url;
+    await newCourse.save();
+
+    res.status(201).json({
+      success: true,
+      message: "Course Added",
     });
   } catch (error) {
     res.status(500).json({
