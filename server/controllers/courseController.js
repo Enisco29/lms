6141df620345ca13b70f9cf75034ec2262c79254
Enisco29 <opeyemioluwafisayo29@gmail.com>
2025-10-22 23:39:01 +0100

    feat: added course controller and routed it sucessfully

---
+++ server/controllers/courseController.js
@@ -0,0 +1,48 @@
+import Course from "../models/Course.js";
+
+//get all courses
+export const getAllCourses = async (req, res) => {
+  try {
+    const courses = await Course.find({ isPublished: true })
+      .select(["-courseContent", "-enrolledStudents"])
+      .populate({ path: "educator" });
+
+    res.status(200).json({
+      success: true,
+      courses,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+//get course by id
+export const getCourseId = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const courseData = await Course.findById(id).populate({ path: "educator" });
+
+    //remove lectureUrl if isPreviewFree is false
+    courseData.courseContent.forEach((chapter) => {
+      chapter.chapterContent.forEach((lecture) => {
+        if (!lecture.isPreviewFree) {
+          lecture.lectureUrl = "";
+        }
+      });
+    });
+
+    res.status(200).json({
+      success: true,
+      courseData,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
